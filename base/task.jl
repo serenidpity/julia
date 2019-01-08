526a32ec190d865dfea82ed24e3c6e61082254de
Jameson Nash <vtjnash@gmail.com>
2019-01-08 17:13:12 -0500

    remove Task :queue state
    The queued state error detection is now handled via the `.queue === nothing` field.

--- base/task.jl
+++ base/task.jl
@@ -363,10 +363,9 @@ end
 global const Workqueue = InvasiveLinkedList{Task}()
 
 function enq_work(t::Task)
-    t.state == :runnable || error("schedule: Task not runnable")
+    (t.state == :runnable && t.queue === nothing) || error("schedule: Task not runnable")
     ccall(:uv_stop, Cvoid, (Ptr{Cvoid},), eventloop())
     push!(Workqueue, t)
-    t.state = :queued
     return t
 end
 
@@ -402,25 +401,25 @@ julia> istaskdone(b)
 true
 ```
 """
-function schedule(t::Task, arg; error=false)
+function schedule(t::Task, @nospecialize(arg); error=false)
     # schedule a task to be (re)started with the given value or exception
     if error
         t.exception = arg
     else
         t.result = arg
     end
-    return enq_work(t)
+    enq_work(t)
+    return t
 end
 
 # fast version of `schedule(t, arg); wait()`
-function schedule_and_wait(t::Task, arg=nothing)
-    t.state == :runnable || error("schedule: Task not runnable")
+function schedule_and_wait(t::Task, @nospecialize(arg)=nothing)
+    (t.state == :runnable && t.queue === nothing) || error("schedule: Task not runnable")
     if isempty(Workqueue)
         return yieldto(t, arg)
     else
         t.result = arg
         push!(Workqueue, t)
-        t.state = :queued
     end
     return wait()
 end
@@ -440,8 +439,7 @@ yield() = (enq_work(current_task()); wait())
 A fast, unfair-scheduling version of `schedule(t, arg); yield()` which
 immediately yields to `t` before calling the scheduler.
 """
-function yield(t::Task, @nospecialize x = nothing)
-    t.state == :runnable || error("schedule: Task not runnable")
+function yield(t::Task, @nospecialize(x=nothing))
     t.result = x
     enq_work(current_task())
     return try_yieldto(ensure_rescheduled, Ref(t))
@@ -455,7 +453,7 @@ called with no arguments. On subsequent switches, `arg` is returned from the tas
 call to `yieldto`. This is a low-level call that only switches tasks, not considering states
 or scheduling in any way. Its use is discouraged.
 """
-function yieldto(t::Task, @nospecialize x = nothing)
+function yieldto(t::Task, @nospecialize(x=nothing))
     t.result = x
     return try_yieldto(identity, Ref(t))
 end
@@ -490,14 +488,12 @@ function ensure_rescheduled(othertask::Task)
         # we failed to yield to othertask
         # return it to the head of the queue to be scheduled later
         pushfirst!(Workqueue, othertask)
-        othertask.state = :queued
     end
-    if ct.state == :queued
+    if ct.queue === Workqueue
         # if the current task was queued,
         # also need to return it to the runnable state
         # before throwing an error
         list_deletefirst!(Workqueue, ct)
-        ct.state = :runnable
     end
     nothing
 end
@@ -505,16 +501,15 @@ end
 function trypoptask()
     isempty(Workqueue) && return
     t = popfirst!(Workqueue)
-    if t.state != :queued
+    if t.state != :runnable
         # assume this somehow got queued twice,
         # probably broken now, but try discarding this switch and keep going
         # can't throw here, because it's probably not the fault of the caller to wait
         # and don't want to use print() here, because that may try to incur a task switch
         ccall(:jl_safe_printf, Cvoid, (Ptr{UInt8}, Int32...),
-            "\nWARNING: Workqueue inconsistency detected: popfirst!(Workqueue).state != :queued\n")
+            "\nWARNING: Workqueue inconsistency detected: popfirst!(Workqueue).state != :runnable\n")
         return
     end
-    t.state = :runnable
     return Ref(t)
 end
 
@@ -548,3 +543,6 @@ if Sys.iswindows()
 else
     pause() = ccall(:pause, Cvoid, ())
 end
+
+
+>>>>>>> f767b67c2d... remove Task :queue state
